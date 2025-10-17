3b4a65d659b73d6f95ec57fb12fd401c07b8de36
foster95 <alicerosefoster95@gmail.com>
2025-10-17 16:19:49 +0100

    add django generic view

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as blog_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.blog, name='home'),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
